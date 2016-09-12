6adbc92cc92d2d9282411035f5915cdf5a2f1f2a
dani van der werf <danivdwerf@gmail.com>
2016-09-12 10:17:55 +0200

    playermovement

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -4,18 +4,37 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour 
 {
     private Rigidbody rigidBody;
+    private float speed;
+    private float jumpheight;
+    private bool isFalling;
 	void Start () 
     {
-        rigidBody = GetComponent<rigidBody>();
+        rigidBody = GetComponent<Rigidbody>();
+        speed = 8f;
+        isFalling = true;
+        jumpheight = 6f;
 	}
 
-	void Update () 
+	private void Update () 
     {
-	    
+        if (Input.GetKeyDown(KeyCode.Space)&&!isFalling)
+        {
+            rigidBody.velocity = new Vector3(0f, jumpheight, 0f);
+            isFalling = true;
+        }
+        Debug.Log(isFalling);
 	}
 
-    void FixxedUpdate()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        if (other.gameObject.CompareTag("Ground")&&isFalling)
+        {
+            isFalling = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.transform.position + transform.forward.normalized * speed * Time.deltaTime);
     }
 }
