e1264d2f72724786757906cdccb2e44a0ade0d41
dani van der werf <danivdwerf@gmail.com>
2016-09-12 09:48:21 +0200

    included Unity file

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    private Rigidbody rigidBody;
+	void Start () 
+    {
+        rigidBody = GetComponent<rigidBody>();
+	}
+
+	void Update () 
+    {
+	    
+	}
+
+    void FixxedUpdate()
+    {
+        
+    }
+}
