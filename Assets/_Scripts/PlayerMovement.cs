b7b79b1e32fae6eb2a288614d7ff423fb4c1552f
dani van der werf <danivdwerf@gmail.com>
2016-09-12 19:51:12 +0200

    Playermovement Jump, Crouch left and right

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -5,14 +5,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rigidBody;
     private float speed;
+    private float side_speed;
     private float jumpheight;
     private bool isFalling;
+
 	void Start () 
     {
         rigidBody = GetComponent<Rigidbody>();
-        speed = 8f;
+        speed = 8f; //make it 20!
         isFalling = true;
-        jumpheight = 6f;
+        jumpheight = 25f;
+        side_speed = 0;
 	}
 
 	private void Update () 
@@ -22,7 +25,27 @@ public class PlayerMovement : MonoBehaviour
             rigidBody.velocity = new Vector3(0f, jumpheight, 0f);
             isFalling = true;
         }
-        Debug.Log(isFalling);
+        if (Input.GetKey(KeyCode.A))
+        {
+            side_speed = -10000;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            side_speed = 10000;
+        }
+        else
+        {
+            side_speed = 0;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.localScale = new Vector3(1, 0.2f, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
 	}
 
     private void OnCollisionEnter(Collision other)
@@ -36,5 +59,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rigidBody.MovePosition(rigidBody.transform.position + transform.forward.normalized * speed * Time.deltaTime);
+        rigidBody.AddForce(rigidBody.transform.right * side_speed);
+        rigidBody.AddForce(0,-5000,0);
     }
 }
